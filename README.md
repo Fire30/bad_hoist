e26ca0d2dd3c34bfaa421249bd9486a5c0ee64c4
Fire30 <tjcorley30@hotmail.com>
2019-12-30 17:21:47 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+bad_hoist
+============
+
+Exploit implementation of
+[CVE-2018-4386](https://bugs.chromium.org/p/project-zero/issues/detail?id=1665).
+Obtains addrof/fakeobj and arbitrary read/write primitives.
+
+Supports PS4 consoles on 6.XX. May also work on older firmware versions,
+but I am not sure. Bug was fixed in firmware 7.00.
---
+++ js/utils.js
@@ -0,0 +1,53 @@
+function debug_log(msg) {
+    // var xhttp = new XMLHttpRequest();
+    // xhttp.open("POST", "/debug/log", false);
+    // xhttp.setRequestHeader("Content-type", "text/html");
+    // xhttp.send(msg);
+    cosole.log(msg);
+}
+
+// The following functions are taken from https://github.com/saelo/jscpwn/:
+//  hex, hexlify, unhexlify, hexdump
+//  Copyright (c) 2016 Samuel Groß
+
+// Return the hexadecimal representation of the given byte.
+function hex(b) {
+    return ('0' + b.toString(16)).substr(-2);
+}
+
+// Return the hexadecimal representation of the given byte array.
+function hexlify(bytes) {
+    var res = [];
+    for (var i = 0; i < bytes.length; i++)
+        res.push(hex(bytes[i]));
+
+    return res.join('');
+}
+
+// Return the binary data represented by the given hexdecimal string.
+function unhexlify(hexstr) {
+    if (hexstr.length % 2 == 1)
+        throw new TypeError("Invalid hex string");
+
+    var bytes = new Uint8Array(hexstr.length / 2);
+    for (var i = 0; i < hexstr.length; i += 2)
+        bytes[i / 2] = parseInt(hexstr.substr(i, 2), 16);
+
+    return bytes;
+}
+
+function hexdump(data) {
+    if (typeof data.BYTES_PER_ELEMENT !== 'undefined')
+        data = Array.from(data);
+
+    var lines = [];
+    for (var i = 0; i < data.length; i += 16) {
+        var chunk = data.slice(i, i + 16);
+        var parts = chunk.map(hex);
+        if (parts.length > 8)
+            parts.splice(8, 0, ' ');
+        lines.push(parts.join(' '));
+    }
+
+    return lines.join('\n');
+}
